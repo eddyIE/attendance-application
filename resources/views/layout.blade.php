6a4dc82c5000232ef3b5eb61c7800b0068eded41
eddyIE <trandainghia456@gmail.com>
2022-01-13 12:40:35 +0700

    update paging function

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -226,6 +226,7 @@
     <script src="{{ asset('js/datatables-buttons/dataTables.buttons.min.js') }}"></script>
     <script src="{{ asset('js/datatables-buttons/buttons.print.min.js') }}"></script>
     <script src="{{ asset('js/datatables-buttons/buttons.colVis.min.js') }}"></script>
+    <script src="{{ asset('js/datatables-bs4/dataTables.bootstrap4.min.js') }}"></script>
     <!-- overlayScrollbars -->
     <script src="{{ asset('js/overlayScrollbars/jquery.overlayScrollbars.min.js') }}"></script>
     <!-- AdminLTE App -->

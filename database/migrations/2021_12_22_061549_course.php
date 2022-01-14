37747997edd6eeb30bee15944125b0419ee62f79
eddyIE <trandainghia456@gmail.com>
2022-01-14 11:22:53 +0700

    update database

--- database/migrations/2021_12_22_061549_course.php
+++ database/migrations/2021_12_22_061549_course.php
@@ -17,10 +17,10 @@ class Course extends Migration
             $table->string("id", 36);
             $table->string("name", 50);
             $table->decimal("credit_hours", 10, 2)->unsigned();
-            $table->integer("finished_lesson")->default(0);
-            $table->decimal("finished_hour", 10, 2)->default(0)->unsigned();
             $table->string("class_id", 36);
             $table->string("subject_id", 36);
+            $table->decimal("finished_hour", 10, 2)->default(0)->unsigned();
+            $table->integer("finished_lesson")->default(0);
             $table->string("created_by", 50);
             $table->dateTime("created_at")->useCurrent();
 
